2e3d4197a3a63b33fe23751e253f5731ffad46dd
Irshad <mhdirshad2k2@gmail.com>
2023-08-09 19:31:31 +0530

    server added for registration page

--- http-server/index.js
+++ http-server/index.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const fs = require('fs');
+const args = require('minimist')(process.argv.slice(2));
 
 let homeContent ='';
 let projectContent = '';
+let registrationContent = '';
 
 fs.readFile('home.html', (err, home) => {
     if (err) throw err
@@ -14,6 +16,11 @@ fs.readFile('project.html', (err, project) => {
     projectContent = project;
 });
 
+fs.readFile('registration.html', (err, registration) =>{
+    if (err) throw err
+    registrationContent = registration;
+})
+
 http.createServer((request, response) => {
     let url = request.url;
     response.writeHead(200, {'Content-Type' : 'text/html'});
@@ -23,9 +30,14 @@ http.createServer((request, response) => {
             response.end();
             break;
         
+        case "/registration" :
+            response.write(registrationContent);
+            response.end();
+            break;
+
         default :
             response.write(homeContent);
             response.end();
             break;
     }
-}).listen(3000);
+}).listen(args.port);
