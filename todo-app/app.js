9657d618dafaef27846870665f11da33e4e1c1ad
Irshad <mhdirshad2k2@gmail.com>
2023-08-21 22:13:19 +0530

    Update prod script

--- todo-app/app.js
+++ todo-app/app.js
@@ -2,10 +2,27 @@ const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
+const path = require("path");
 app.use(bodyParser.json());
 
-app.get("/", function (request, response) {
-  response.send("Hello World");
+// setting ejs view
+app.set("view engine", "ejs");
+
+// eslint-disable-next-line no-undef
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", async function (request, response) {
+  const allTodos = await Todo.getTodos();
+
+  if (request.accepts("html")) {
+    response.render("index", {
+      allTodos,
+    });
+  } else {
+    response.json({
+      allTodos,
+    });
+  }
 });
 
 app.get("/todos", async function async(_request, response) {
