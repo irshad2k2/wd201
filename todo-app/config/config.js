2ae6e7c5ce7b1b6c47df70e2673cbfddafc3fc5b
irshad2k2 <mhdirshad2k2@gmail.com>
2024-07-12 23:05:28 +0530

    db_config updated"

--- todo-app/config/config.js
+++ todo-app/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     username: process.env.DEV_USERNAME,
     password: process.env.DEV_PASSWORD,
     database: process.env.DEV_DATABASE,
-    host: process.env.DEV_HOST,
+    host: "127.0.0.1",
     dialect: "postgres",
     logging: false,
   },
